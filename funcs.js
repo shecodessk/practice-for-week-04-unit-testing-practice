053f02f25d33c74d518507a0574ef005980fbed0
StephanyKleen <stephanykleen@gmail.com>
2023-07-28 10:38:13 -0700

    Using unit test specs to create the func code block and pass mocha testing

--- funcs.js
+++ funcs.js
@@ -1,13 +1,21 @@
 function isFive(num) {
-  // Your code here
+  return num === 5 ? true : false;
 }
 
 function isOdd(number) {
-  // Your code here
+  if(typeof number !== 'number'){
+    throw new Error('Not a number')
+  }
+
+  return number % 2 !== 0 ? true : false;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  const array = [];
+    for(let i = min; i <= max; i+= step){
+        array.push(i);
+    }
+  return array;
 }
 
 
